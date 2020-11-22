7a77f2edf2efab299c65bbc92fb8b195e7e0f05f
WiktorRX <wiktorm@live.com>
2020-11-22 19:10:01 +0000

    11222009

--- zal/transportujFlota.hpp
+++ zal/transportujFlota.hpp
@@ -4,8 +4,13 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
+  unsigned int transit = 0;
+  while (dotransportu)
+  {
     Stocznia Kuat{}
     Statek* ISD = Kuat();
+    transit += ISD.transportuj();
+  }
   
     return 0;
 }
