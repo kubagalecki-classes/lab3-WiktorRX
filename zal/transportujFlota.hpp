2ce880b482403b70ffc4afcf136577de7be2e7e8
WiktorRX <wiktorm@live.com>
2020-11-22 21:12:06 +0000

    11222212

--- zal/transportujFlota.hpp
+++ zal/transportujFlota.hpp
@@ -5,12 +5,21 @@
 unsigned int transportujFlota(unsigned int towar)
 {
   unsigned int transit = 0;
-  while (dotransportu)
+  unsigned int liczba_zaglowcow = 0;
+
+    while (transit < towar)
   {
     Stocznia Kuat{}
     Statek* ISD = Kuat();
     transit += ISD.transportuj();
+    
+    Zaglowiec* wsk_zaglowca = dynamic_cast<Zaglowiec*>(ISD);
+    if(wsk_zaglowca !=0)
+      liczba_zaglowcow +=1;
+
+    delete ISD;
+
   }
   
-    return 0;
+    return liczba_zaglowcow;
 }
